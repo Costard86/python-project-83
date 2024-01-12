f6dfba33feebcc1a85135dee2b6d0c1a1fed3b48
Costard86 <Costard86@gmail.com>
2024-01-12 13:07:12 +0700

    Step 3 fixed database.sql

--- database.sql
+++ database.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS public.urls
 (
-    id bigint NOT NULL,
-    name character varying(100) COLLATE pg_catalog."default" NOT NULL,
-    created_at timestamp without time zone NOT NULL,
+    id bigint NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 9223372036854775807 CACHE 1 ),
+    name character varying(255) COLLATE pg_catalog."default" NOT NULL,
+    created_at timestamp without time zone NOT NULL DEFAULT now(),
     CONSTRAINT urls_pkey PRIMARY KEY (id)
 )
